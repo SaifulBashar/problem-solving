523e5cbd0d690b1144dd27d0999fbf728d731a47
Saiful Bashar <robal.bashar59@gmail.com>
2023-09-21 10:31:46 +0600

    leetcode-217

---
+++ contains-duplicate.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const map = new Map();
+    for(const i of nums){
+        if(map.has(i)){
+            return true
+        }
+        map.set(i,i)
+    }
+    return false
+};
